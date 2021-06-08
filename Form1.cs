c36736ac25f0ac61103a840ad766ac61bd2f4749
Denis <byd.mak@gmail.com>
2021-06-08 16:49:25 +0300

    first update

---
+++ Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CheckBlockCheir
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            InitializeComponent();
+            OpenFileDialog OPF = new OpenFileDialog();
+            OPF.Multiselect = true;
+            OPF.Filter = "Файлы dat|*.dat";
+            if (OPF.ShowDialog() == DialogResult.OK)
+            {
+                
+            }
+            string[] lines = File.ReadAllLines(OPF.FileName);
+            foreach (string i in lines)
+            {
+                string str = i;
+                string str1 = "name";
+                if (str.Contains(str1))
+                    Console.WriteLine(str, str1);
+            }
+        }
+    }
+}
