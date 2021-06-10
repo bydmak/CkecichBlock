41b8bb419e0c6f77b09dcc081775bf2a82bf9e10
Denis <byd.mak@gmail.com>
2021-06-10 17:52:56 +0300

    double

--- Form1.cs
+++ Form1.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -23,19 +24,41 @@ namespace CheckBlockCheir
             InitializeComponent();
             OpenFileDialog OPF = new OpenFileDialog();
             OPF.Multiselect = true;
+            string lastname = "";
             OPF.Filter = "Файлы dat|*.dat";
             if (OPF.ShowDialog() == DialogResult.OK)
             {
                 
             }
+
             string[] lines = File.ReadAllLines(OPF.FileName);
+            //StreamWriter f = new StreamWriter("test.txt", true);
             foreach (string i in lines)
             {
+               // string pattern = @"\w[name]\w";
+                //Regex rg = new Regex(pattern);
+                //MatchCollection matchedAuthors = rg.Matches(i);
+                //for (int count = 0; count < matchedAuthors.Count; count++)
+                //    Console.WriteLine(matchedAuthors[count].Value);
+
                 string str = i;
                 string str1 = "name";
                 if (str.Contains(str1))
-                    Console.WriteLine(str, str1);
+                   // f.WriteLine(str, str1);
+                    Console.WriteLine(str);
+
+
+                lastname = str.Substring(str.IndexOf("name"));
+                //f.WriteLine(lastname);
             }
+
+           // Console.WriteLine(lastname);
+            label3.Text = lastname;
+            label4.Text = OPF.FileName;
+
+
         }
+
+
     }
 }
